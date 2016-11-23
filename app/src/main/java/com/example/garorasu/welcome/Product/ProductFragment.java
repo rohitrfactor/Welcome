caf31fb88c1c2ec3bed8a529b99fe2e2c2d101af
rohitrfactor <rohitrfactor@gmail.comm>
2016-11-23 14:05:42 +0530

    Carousel in Product Section

--- app/src/main/java/com/example/garorasu/welcome/Product/ProductFragment.java
+++ app/src/main/java/com/example/garorasu/welcome/Product/ProductFragment.java
@@ -5,18 +5,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.garorasu.welcome.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 
 public class ProductFragment extends Fragment {
 
-
+    private CarouselView carouselView;
+    int[] sampleImages = {R.drawable.testbook,R.drawable.book_grass,R.drawable.product,R.drawable.laptop};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product, container, false);
+        carouselView = (CarouselView) view.findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
         return view;
     }
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
